acde85e6f8883560214932d08890d7cd26500ec5
ntimoo <141534511+ntimoo@users.noreply.github.com>
2023-08-21 15:58:26 +0100

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,8 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println(" HELLO CHRIZPAPA"); 
+        System.out.println(" HELLO CHRIZPAPA");
+        System.out.println(" CHRIZPAPA USING THE PULL COMMAND");
         // Hello World!
     }
 }
